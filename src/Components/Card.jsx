3e758be71e6316d25f938f7c4feb105f6c664456
ayyushhere <ayushkumar0211a@gmail.com>
2025-09-02 23:19:25 +0530

    Made changes in css of Card component

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,22 +2,31 @@ import { FcLike } from "react-icons/fc";
 
 const Card = (props) => {
   let course = props.course;
-  console.log(course.image.alt)
+  let likedCourses = props.likedCourses;
+  let setLikedCourses = props.setLikedCourses;
+
+  function clickHandler(){
+    //logic
+    if(likedCourses.includes(course.id)){
+      setLikedCourses( (prev) => prev.filter((cid !== course.id)) );
+    }
+  }
+
   return (
     <div className="w-[300px] bg-black text-white rounded-md overflow-hidden">
-      <div>
+      <div className="relative">
         <img
           src={course.image.url}
           alt={course.image.alt} />
+        <div className="w-[30px] h-[30px] bg-white rounded-full absolute right-2 bottom-2 grid place-items-center">
+          <button onClick={clickHandler}>
+            <FcLike fontSize={"1.50rem"}/>
+          </button>
+        </div>
       </div>
-      <div>
-        <button>
-          <FcLike />
-        </button>
-      </div>
-      <div>
-        <p>{course.title}</p>
-        <p>{course.description}</p>
+      <div className="p-4 ">
+        <p className="text-white font-semibold text-lg leading-6">{course.title}</p>
+        <p className="text-white overflow-auto mt-2">{course.description}</p>
       </div>
     </div>
   )
