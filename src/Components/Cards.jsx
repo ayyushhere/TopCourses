e2b5c81ea99615d2357a8b0e00e7bc394b6bdffb
ayyushhere <ayushkumar0211a@gmail.com>
2025-09-02 22:14:00 +0530

    Made changes in css of Cards component

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -15,7 +15,7 @@ const Cards = (props) => {
   }
 
   return (
-    <div>
+    <div className='flex flex-wrap justify-center gap-4 m-5'>
       {
         getCourses().map((course) => (
           <Card key={course.id} course={course} />
