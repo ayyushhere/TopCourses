629b9262b82fffda256c898ffc64f6aa2acd4dfe
ayyushhere <ayushkumar0211a@gmail.com>
2025-09-03 23:46:36 +0530

    Made changes in css of Filter jsx

--- src/Components/Filter.jsx
+++ src/Components/Filter.jsx
@@ -6,7 +6,7 @@ const Filter = (props) => {
         {
             filterData.map((data) => {
                 return <button key={data.id} className={`
-            text-lg px-2 py-1 rounded-md bg-black border-2 bg-opacity-60 hover:bg-opacity-50 text-white transition-all duration-300 font-medium`}
+            text-lg p-5 rounded-md bg-black border-white border-2 bg-opacity-60 hover:bg-gray-800 text-white transition-all duration-300 ease-in-out font-medium cursor-pointer`}
                 >{data.title} </button>;
             })
         }
